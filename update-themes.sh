a8ae54211781f8ec2d50918825231c32b251b0fa
Alex Cruz <mazdarider23@gmail.com>
2016-12-28 05:34:48 +0100

    update theme script
    Change-Id: I80e1662425e104dfb2f1f2acdf6eb35af2217b44

--- update-themes.sh
+++ update-themes.sh
@@ -14,8 +14,7 @@
 # limitations under the License.
 
 WORKING_DIR=$1
-RESOURCES_REPO='https://github.com/Evervolv/android_developers_themes_resources.git'
-THEME_RESOURCES_DIR='developers/themes/resources'
+THEME_RESOURCES_DIR='theme-resources'
 
 if [ $# -ne 1 ] || [ ! -d $WORKING_DIR ]; then
   echo "usage: ./update-themes.sh ABSOLUTE_PATH_TO_REPO"
@@ -65,48 +64,35 @@ function copy_all () {
   done
 }
 
-declare -a root=('abi' 'bionic' 'art' 'bootable' 'build' 'dalvik' 'development' 'device'
-                 'external' 'frameworks' 'hardware' 'kernel' 'libcore' 'libnativehelper'
-                 'manifest' 'ndk' 'out' 'packages' 'pdk' 'prebuilts' 'sdk' 'system' 'tools' 'vendor')
+declare -a root=('abi' 'bionic' 'art' 'bootable' 'build' 'dalvik' 'development' 'device' 'external' 'frameworks' 'hardware' 'kernel' 'libcore' 'libnativehelper' 'manifest' 'ndk' 'out' 'packages' 'pdk' 'prebuilts'
+                 'sdk' 'system' 'theme-resources' 'tools' 'vendor')
 
-declare -a frameworks=('api' 'cmds' 'data' 'docs' 'drm' 'graphics' 'include' 'keystore'
-                       'libs' 'location' 'media' 'native' 'nfc-extras' 'obex' 'opengl'
-                       'policy' 'rs' 'samples' 'sax' 'security-bridge' 'services'
-                       'telecomm' 'telephony' 'test-runner' 'tests' 'tools' 'wifi')
+declare -a frameworks=('api' 'cmds' 'data' 'docs' 'drm' 'graphics' 'include' 'keystore' 'libs' 'location' 'media' 'native' 'nfc-extras' 'obex' 'opengl' 'policy' 'rs' 'samples' 'sax' 'security-bridge' 'services' 'telecomm'
+                       'telephony' 'test-runner' 'tests' 'tools' 'wifi')
 
-declare -a common=('.classpath' '.git' '.gitignore' '.idea' '.project' 'assets' 'build.gradle'
-                   'src' 'tests' 'Android.mk' 'AndroidManifest.xml' 'CleanSpec.mk' 'lint.xml'
-                   'MODULE_LICENSE_APACHE2' 'NOTICE' 'preloaded-classes' 'proguard.cfg' 'proguard.flags'
-                   'proguard-test.flags' 'wrap_alpha.py' 'tools' 'tests' 'extensions_src' 'mkprojectfile'
-                   'project.properties' 'hero.png' 'LICENSE' 'README.md' 'script' 'jni' 'res_p' 'sounds'
-                   'src_pd' 'src_pd_gcam' 'tests_camera' 'version.mk' 'COPYING' 'ReadMe.md' 'WallpaperPicker'
-                   'protos' 'util' 'fill_screens.py' 'print_db.py' 'update_gallery_files.py' 'update_system_wallpaper_cropper.py'
-                   'cyanoden-dsp' 'emailcommon' 'transition' 'TestCommon' 'provider_src' 'gallerycommon' 'jni_jpegstream' 'apptests')
+declare -a common=('common.mk' '.classpath' '.git' '.gitignore' '.idea' '.project' 'assets' 'build.gradle' 'src' 'tests' 'Android.mk' 'CleanSpec.mk' 'lint.xml' 'MODULE_LICENSE_APACHE2' 'NOTICE' 'preloaded-classes'
+                   'proguard.cfg' 'proguard.flags' 'proguard-test.flags' 'wrap_alpha.py' 'tools' 'tests' 'extensions_src' 'mkprojectfile' 'project.properties' 'hero.png' 'LICENSE' 'README.md' 'script' 'jni' 'res_p' 'sounds'
+                   'src_pd' 'src_pd_gcam' 'tests_camera' 'version.mk' 'COPYING' 'ReadMe.md' 'WallpaperPicker' 'protos' 'util' 'fill_screens.py' 'print_db.py' 'update_gallery_files.py' 'update_system_wallpaper_cropper.py'
+                   'cyanoden-dsp' 'emailcommon' 'transition' 'TestCommon' 'provider_src' 'gallerycommon' 'jni_jpegstream' 'apptests' 'libs' 'info_lookup' 'src_plugin' 'perf-tests' 'app-perf-tests' 'src-N' 'src-pre-N' 'build-app.gradle' 'build-library.gradle' 'local.properties' 'gradlew.bat' 'gradlew' 'gradle' 'build.gradle' '.idea' 'settings.gradle' 'gradle.properties' 'proguard-rules.pro' 'v' '*.jar' 'build' 'ForceProguard.mk' 'proguard-release.flags' 'Readme.md')
 
-declare -a packages=('BackupRestoreConfirmation' 'CaptivePortalLogin' 'DefaultContainerService'
-                     'ExternalStorageProvider' 'FakeOemFeatures' 'FusedLocation' 'InputDevices'
-                     'Keyguard' 'PrintSpooler' 'SettingsProvider' 'SharedStorageBackup' 'Shell'
-                     'VpnDialogs' 'WAPPushManager' 'WAPPushManager' 'WallpaperCropper' 'services')
+declare -a packages=('StatementService' 'BackupRestoreConfirmation' 'CaptivePortalLogin' 'DefaultContainerService' 'ExternalStorageProvider' 'FakeOemFeatures' 'FusedLocation' 'InputDevices' 'Keyguard' 'PrintSpooler' 'SettingsProvider'
+                     'SharedStorageBackup' 'Shell' 'VpnDialogs' 'WAPPushManager' 'WAPPushManager' 'WallpaperCropper' 'services' 'WallpaperBackup' 'PrintRecommendationService' 'Osu' 'MtpDocumentsProvider' 'ExtShared' 'ExtServices' 'CtsShim')
 
-declare -a res=('assets' 'menu*' 'values-*' 'xml*' 'interpolator' 'raw*' 'anim' 'animator' 'transition' 'MakeJavaSymbols.sed')
+declare -a res=('Android.mk' 'AndroidManifest.xml' 'MODULE_LICENSE_APACHE2' 'NOTICE' 'lint.xml' 'assets' 'menu*' 'values-*' 'xml*' 'interpolator' 'raw*' 'anim*' 'animator' 'transition' 'MakeJavaSymbols.sed' 'tests' 'src' 'build.gradle' 'proguard.flags' 'setting' 'gradlew' 'gradlew.bat')
 
-declare -a values=('all_search_engines.xml' 'appmsg_colors.xml' 'arrays.xml' 'attrs.xml' 'attrs_manifest.xml'
-                   'bookmarks_icons.xml' 'bools.xml' 'config.xml' 'defaults.xml' 'donottranslate.xml' 'donottranslate_config.xml'
-                   'aliases.xml' 'crop_colors.xml' 'dslv_attrs.xml' 'animation_constants.xml' 'donottranslate-cldr.xml'
-                   'donottranslate-maps.xml' 'donottranslate-names.xml' 'donottranslate_material.xml' 'donottranslate-search_engines.xml'
-                   'du_arrays.xml' 'du_strings.xml' 'du_symbols.xml' 'ev_arrays.xml' 'ev_strings.xml' 'ev_symbols.xml'
-                   'fractions.xml' 'ids.xml' 'integers.xml' 'internal.xml' 'keys.xml' 'lland_config.xml' 'lland_strings.xml'
-                   'plurals.xml' 'public.xml' 'strings.xml' 'symbols.xml' 'vpi_attrs.xml' 'vpi_defaults.xml' 'cm_arrays.xml'
-                   'cm_plurals.xml' 'cm_strings.xml' 'custom_strings.xml' 'custom_arrays.xml' 'vpi__defaults.xml' 'vpi__attrs.xml'
-                   'nontranslatables.xml' 'accountprovider.xml' 'constants.xml' 'codeaurora_strings.xml' 'filtershow_ids.xml'
+declare -a values=('all_search_engines.xml' 'appmsg_colors.xml' 'arrays.xml' 'attrs_manifest.xml' 'bookmarks_icons.xml' 'bools.xml' 'config.xml' 'defaults.xml' 'donottranslate.xml' 'donottranslate_config.xml'
+                   'aliases.xml' 'crop_colors.xml' 'dslv_attrs.xml' 'animation_constants.xml' 'donottranslate-cldr.xml' 'donottranslate-maps.xml' 'donottranslate-names.xml' 'donottranslate_material.xml'
+                   'donottranslate-search_engines.xml' 'du_arrays.xml' 'du_strings.xml' 'du_symbols.xml' 'ev_arrays.xml' 'ev_strings.xml' 'ev_symbols.xml' 'fractions.xml' 'ids.xml' 'integers.xml' 'internal.xml'
+                   'keys.xml' 'lland_config.xml' 'lland_strings.xml' 'plurals.xml' 'public.xml' 'strings.xml' 'symbols.xml' 'vpi_attrs.xml' 'vpi_defaults.xml' 'cm_arrays.xml' 'cm_plurals.xml' 'cm_strings.xml' 
+                   'custom_strings.xml' 'custom_arrays.xml' 'vpi__defaults.xml' 'vpi__attrs.xml' 'nontranslatables.xml' 'accountprovider.xml' 'constants.xml' 'codeaurora_strings.xml' 'filtershow_ids.xml' 
                    'filtershow_strings.xml' 'filtershow_values.xml' 'filtershow_values_attrs.xml')
 
-declare -a core=('java' 'jni' 'tests')
+declare -a core=('java' 'jni' 'tests' 'src')
 
-declare -a aosp_packages=('Browser' 'Calculator' 'Calendar' 'Camera2' 'Contacts' 'ContactsCommon' 'DeskClock' 'Dialer' 'Email' 'Gallery2' 'InCallUI'
-                           'Launcher3' 'Mms' 'PhoneCommon' 'Settings' 'Stk')
+declare -a theme_packages=('Calculator' 'Calendar' 'Camera2' 'Contacts' 'ContactsCommon' 'DeskClock' 'Dialer' 'EVToolbox'
+                           'EVUpdater' 'ExactCalculator' 'Gallery2' 'Launcher3' 'Messaging' 'PhoneCommon' 'Settings' 'Stk' 'Sudo')
 
-declare -a vendor_packages=('EVToolbox' 'EVUpdater' 'ThemeChooser')
+declare -a extra=('java' '.idea' 'gradle' '.gitignore' 'build.gradle' 'gradlew' 'gradlew.bat' 'local.properties' 'proguard-rules.pro' 'proguard.flags')
 
 cd $WORKING_DIR
 echo "Removing files so we can clean sync"
@@ -115,19 +101,40 @@ cd .repo
 rm -rf local_manifests
 cd ..
 echo "Repo Syncing........."
-repo sync >> /dev/null
+repo sync --force-sync >> /dev/null
 if [ $? -eq 0 ]; then
   echo "Repo Sync success"
 else
   echo "Repo Sync failure"
   exit 1
 fi
+echo "Cloning Theme Resources repo"
+git clone https://github.com/elginsk8r/theme-resources.git
 echo "Removing unneeded files"
 cd frameworks/base
 delete_useless ${frameworks[@]}
 cd packages
 delete_useless ${packages[@]}
 cd SystemUI
+delete_useless ${common[@]}
+rm -rf docs
+cd res
+delete_useless ${res[@]}
+cd values
+delete_useless ${values[@]}
+cd ../../../SettingsLib
+delete_useless ${common[@]}
+cd res
+delete_useless ${res[@]}
+cd values
+delete_useless ${values[@]}
+cd ../../../DocumentsUI
+delete_useless ${common[@]}
+cd res
+delete_useless ${res[@]}
+cd values
+delete_useless ${values[@]}
+cd ../../../EasterEgg
 delete_useless ${common[@]}
 cd res
 delete_useless ${res[@]}
@@ -141,17 +148,20 @@ cd res
 delete_useless ${res[@]}
 cd values
 delete_useless ${values[@]}
-rm -rf  $WORKING_DIR/$THEME_RESOURCES_DIR/frameworks
 cd $WORKING_DIR
-process_all ${aosp_packages[@]}
-process_all ${vendor_packages[@]}
+process_all ${theme_packages[@]}
 echo "Cleaning target folders"
 rm -rf $THEME_RESOURCES_DIR/packages
+rm -rf $THEME_RESOURCES_DIR/frameworks
 mkdir $THEME_RESOURCES_DIR/packages
 mkdir $THEME_RESOURCES_DIR/packages/apps
+mkdir $THEME_RESOURCES_DIR/packages/apps/Dialer
+mkdir $THEME_RESOURCES_DIR/packages/apps/Dialer/InCallUI
 mkdir $THEME_RESOURCES_DIR/frameworks
 echo "Copying all files to $WORKING_DIR/$THEME_RESOURCES_DIR"
 copy_all ${theme_packages[@]}
 cp -r $WORKING_DIR/frameworks/base/core $WORKING_DIR/frameworks/base/packages $WORKING_DIR/$THEME_RESOURCES_DIR/frameworks
 cd $WORKING_DIR/$THEME_RESOURCES_DIR
-echo "Theme resources updated"
+cd $WORKING_DIR/$THEME_RESOURCES_DIR/packages/apps/Dialer/InCallUI
+delete_useless ${res[@]}
+cd $WORKING_DIR/$THEME_RESOURCES_DIR
